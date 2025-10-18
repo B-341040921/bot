7309c9b3cff20e3587cc0e7edcc7fe171c5d5714
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 13:47:33 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -61,13 +61,13 @@ async def on_message(message):
         response_json = response.json()
         channel = client.get_channel(message.channel.id)
         if "choices" in response_json and len(response_json["choices"]) > 0 and "message" in response_json["choices"][0] and "content" in response_json["choices"][0]["message"]:
-          await channel.response.send_message(response_json["choices"][0]["message"]["content"])
+          await ctx.response.send_message(response_json["choices"][0]["message"]["content"])
         else:
-          await channel.response.send_message("Could not get a valid response from the API.")
+          await ctx.response.send_message("Could not get a valid response from the API.")
       except:
-        await channel.response.send_message("API on cooldown")
+        await ctx.response.send_message("API on cooldown")
     else:
-      await channel.response.send_message("not authorized")
+      await ctx.response.send_message("not authorized")
 
 auth=["pvz_watermelus100","watermelon.11.","prime_minister_of_egg","sesruirnuien","lgcool2"]
 user_list=["budo_1","saket","jotan_0200","elenorsilly","zepz","1yme"]
