c1702811462c7afb19b9f429d32b11df7fddf32b
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 00:26:59 -0500

    Create Web_server.py

---
+++ Web_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+  def home():
+    return "a"
+
+def run_web_server():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run_web_server)
+    t.start()
