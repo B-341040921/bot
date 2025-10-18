040edfe16138a8e7068a3bef377fcd5e9a833b2a
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 01:58:57 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,53 +31,41 @@ async def check_user(interaction: discord.Interaction, user: str):
   else:
       await interaction.response.send_message("not authorized")
 
-@tree.command(name="prompt", description="use melon AI (beta)")
-async def prompt(interaction: discord.Interaction, text: str, debug: bool=False):
-  if str(interaction.user) in auth:
-    try:
-      response = requests.post(
-      url="https://openrouter.ai/api/v1/chat/completions",
-      headers={
-        "Authorization": "Bearer "+os.environ.get(API),
-        "Content-Type": "application/json",
-       },
-       data=json.dumps({
-       "preset": "@preset/wide-agent",
-       "model": "meta-llama/llama-3.2-3b-instruct:free",
-        "messages": [
-            {
-            "role": str(interaction.user),
-            "content": text
-             }
-        ],
-
-      })
-     )
-      if debug:
-        await interaction.response.send_message("[NEUTRAL]",response)
-        await interaction.response.send_message("[TEXT]",response.text)
-      else:
-        response_json = response.json()
-        if "choices" in response_json and len(response_json["choices"]) > 0 and "message" in response_json["choices"][0] and "content" in response_json["choices"][0]["message"]:
-          await interaction.response.send_message(response_json["choices"][0]["message"]["content"])
-        else:
-          await interaction.response.send_message("Could not get a valid response from the API.")
-    except:
-       await interaction.response.send_message("API on cooldown")
-  else:
-    await interaction.response.send_message("not authorized")
-
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-    if message.content.lower() in keywords:
-      await message.channel.send('<@&1422797659026231347> Alert: possible raid, report sent to DirWIDE and DirINT')
-      member =  client.get_user(936848029179326474)
-      await member.send(f"{message.author} sent {message.content}")
-      await message.delete()
+  if bot.user.mentioned_in(message):
+    if str(interaction.user) in auth:
+      try:
+        response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+          "Authorization": "Bearer "+os.environ.get("API"),
+          "Content-Type": "application/json",
+         },
+         data=json.dumps({
+         "preset": "@preset/wide-agent",
+         "model": "meta-llama/llama-3.2-3b-instruct:free",
+         "messages": [
+             {
+              "role": str(interaction.user),
+              "content": text
+               }
+          ],
 
+        })
+       )
+          response_json = response.json()
+          if "choices" in response_json and len(response_json["choices"]) > 0 and "message" in response_json["choices"][0] and "content" in response_json["choices"][0]["message"]:
+            await interaction.response.send_message(response_json["choices"][0]["message"]["content"])
+        else:
+          await interaction.response.send_message("Could not get a valid response from the API.")
+      except:
+        await interaction.response.send_message("API on cooldown")
+    else:
+      await interaction.response.send_message("not authorized")
 
 keywords=[os.environ.get("KEYWORDS")]
 auth=["pvz_watermelus100","watermelon.11.","prime_minister_of_egg","sesruirnuien","lgcool2"]
