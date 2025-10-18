9f946391e2962f69a242e2223562f1edcac1d8d6
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 01:00:28 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
+import asyncio
+import requests
+import json
+import nest_asyncio
+from logging import exception
+from google.colab import userdata
+from discord import app_commands
+from discord.ext import commands
 from web_server import keep_alive
 
 intents = discord.Intents.default()
