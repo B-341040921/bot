41414e1424bf73a97938bbcbfb2f4fe83a970789
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 01:47:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -80,7 +80,7 @@ async def on_message(message):
 
 
 keywords=[os.environ.get("KEYWORDS")]
-auth=[os.environ.get("AUTH")]
+auth=["pvz_watermelus100","watermelon.11.","prime_minister_of_egg","sesruirnuien"]
 user_list=["budo_1","saket","jotan_0200","elenorsilly","zepz","1yme"]
 keep_alive()
 client.run(os.environ.get("TOKEN"))
