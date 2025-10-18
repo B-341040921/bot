b0873c2fff4bf35bd2de107349ea742abc63f57b
B1ll1l1l1l1l1l1l1 <watermelon.wide@gmail.com>
2025-10-18 00:51:25 -0500

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -4,8 +4,8 @@ from threading import Thread
 app = Flask(__name__)
 
 @app.route('/')
-  def home():
-    return "a"
+def home():
+  return "a"
 
 def run_web_server():
   app.run(host='0.0.0.0', port=8080)
